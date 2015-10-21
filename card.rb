4d64148456f13ba2d682b09c6004cb365c46c4a6
Yoan Ante <yoanante@gmail.com>
2015-10-21 16:08:43 -0400

    first commit

---
+++ card.rb
@@ -0,0 +1,7 @@
+
+class Card
+  def initialize(suit, rank)
+    @suit = suit
+    @rank = rank
+  end
+end
---
+++ deck.rb
@@ -0,0 +1,23 @@
+require_relative 'card'
+class Deck
+  SUITS = ["hearts", "spades", "diamonds", "clubs"]
+  RANKS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  attr_reader :cards
+
+  def initialize()
+    @cards = []
+    SUITS.each do |suit|
+      RANKS.each do |rank|
+        i = Card.new(suit, rank)
+        @cards.push(i)
+      end
+    end
+  end
+
+  def shuffle(deck)
+    deck.shuffle
+  end
+
+end
+
+class
